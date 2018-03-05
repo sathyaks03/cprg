ec4ed062d1ec54c54486afd7dbf9e68b8fd5d0e0
sathyaks03 <37013515+sathyaks03@users.noreply.github.com>
2018-03-05 11:45:35 +0530

    Update q10.c

--- set1/q10.c
+++ set1/q10.c
@@ -5,14 +5,18 @@ int main()
     int num,count=0;
     
      scanf("%d",&num);
+    if(num==0)
+        printf("1");
+    else
+    {
     while(num>0)
     {
        num=num/10;
        count++;
     }
-     
+    
      printf("%d",count);
-     
+    } 
     
     return 0;
 }
