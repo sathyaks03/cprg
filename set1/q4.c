450d70762458a8eb6d3503c028d88a525542f160
sathyaks03 <37013515+sathyaks03@users.noreply.github.com>
2018-03-05 11:14:49 +0530

    Create q4.c

---
+++ set1/q4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+     char a;
+     scanf("%c",&a);
+     if((a>=65&&a<=90)||(a>=97&&a<=122))
+     printf("Alphabet");
+     else
+     printf("No");
+    return 0;
+}
+
