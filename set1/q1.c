c887c162b77ae96c6e6a4f91857b1b136e62ae72
sathyaks03 <37013515+sathyaks03@users.noreply.github.com>
2018-03-05 11:05:38 +0530

    Update q1.c

--- set1/q1.c
+++ set1/q1.c
@@ -5,7 +5,7 @@ int main()
      int a;
     scanf("%d",&a);
     if(a==0) printf("zero");
-else if(a<0) printf("negative");
-else printf("positive");
+else if(a>0) printf("positive");
+else printf("negative");
     return 0;
 }
