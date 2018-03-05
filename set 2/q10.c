0f6fe1822e99eaf5cbfdc2dc943e039cb23fe814
sathyaks03 <37013515+sathyaks03@users.noreply.github.com>
2018-03-05 12:04:25 +0530

    Create q10.c

---
+++ set 2/q10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,num;
+    
+     scanf("%d",&num);
+    
+    for(i=1;i<=5;i++)
+    {
+       printf("%d ",i*num);
+    }
+       
+    
+    
+    return 0;
+}
