0ea187a14bdfb67438182b94d29c283fe356a813
sathyaks03 <37013515+sathyaks03@users.noreply.github.com>
2018-03-05 11:23:20 +0530

    Create q6.c

---
+++ set1/q6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+     scanf("%d",&a);
+     if((a%4==0)||(a%100==0&&a%400==0))
+     printf("Yes");
+     
+    else
+    printf("No");
+    return 0;
+}
